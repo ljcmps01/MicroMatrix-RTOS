a931f39740825863587663486984d59b41acc436
acampos7 <acampos@mapertech.com>
2025-10-14 23:44:30 -0300

    add bitris struct and basic landed state

--- src/apps/bitris/bitris.c
+++ src/apps/bitris/bitris.c
@@ -16,14 +16,37 @@ typedef enum {
     BITRIS_GAMEOVER
 }BitrisState_t;
 
+
+typedef struct {
+    uint8_t pos;
+    Direction_t direction;
+    uint8_t level;
+    uint16_t lines_cleared;
+    uint8_t gamescreen[8];
+    BitrisState_t state;
+    uint8_t max_level;
+} BitrisScreen_t;
+
+BitrisScreen_t BitrisInit(){
+    BitrisScreen_t bitris;
+    bitris.level = 1;
+    bitris.lines_cleared = 0;
+    bitris.max_level=8;
+    for(size_t i=0;i<8;++i){
+        bitris.gamescreen[i] = 0x00;
+    }
+    bitris.state = BITRIS_IDLE;
+    bitris.direction = RIGHT;
+    return bitris;
+}
+
 Button sw2,sw3;
-uint8_t gamescreen[8] = {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
-BitrisState_t state = BITRIS_IDLE;
+BitrisScreen_t bitris;
 
 void ButtonHandler(const Button *btn, ButtonEvent_t event){
     switch(event) {
         case BUTTON_EVENT_SHORT: 
-            state = BITRIS_FALLING;
+            bitris.state = BITRIS_FALLING;
             break;
         default: break;
     }
@@ -31,30 +54,35 @@ void ButtonHandler(const Button *btn, ButtonEvent_t event){
 
 void vBitrisTask(void *pvParameters){
     Matrix_t *matrix = GetMatrix();
-    Direction_t direction = RIGHT;
-    uint8_t *player = &gamescreen[0];
     size_t pos = 0;
     for(;;)
     {
-        switch(state){
+        switch(bitris.state){
             case BITRIS_IDLE:           // Player moving left and right
-                Matrix_Clear(matrix);
-                *player = (1<<pos);
-                direction?pos++:pos--;
-                direction = (pos == 7)?LEFT:(pos == 0)?RIGHT:direction;
                 
-                load_output(matrix,gamescreen);
+                bitris.gamescreen[0] = (1<<pos);
+                bitris.direction?pos++:pos--;
+                bitris.direction = (pos == 7)?LEFT:(pos == 0)?RIGHT:bitris.direction;
+                
+                load_output(matrix,bitris.gamescreen);
                 vTaskDelay(pdMS_TO_TICKS(SPEED));
                 break;
             case BITRIS_FALLING:        // Player falling    
-                for(size_t i=0;i<8;++i){
-                    gamescreen[i] = *player;
-                    load_output(matrix,gamescreen);
+                for(size_t i=0;i<(bitris.max_level-bitris.level);++i){
+                    bitris.gamescreen[i] = (1<<pos);
+                    load_output(matrix,bitris.gamescreen);
                     vTaskDelay(pdMS_TO_TICKS(SPEED/2));
                 }
-                state=BITRIS_IDLE;    
+                for(size_t i=1;i<(bitris.max_level-bitris.level);++i){
+                    bitris.gamescreen[i] = 0x00;
+                }
+                bitris.state=BITRIS_LANDED;    
                 break;
             case BITRIS_LANDED:         // Player landed
+                bitris.gamescreen[bitris.max_level-bitris.level]|=bitris.gamescreen[0];
+                load_output(matrix,bitris.gamescreen);
+                bitris.state=BITRIS_IDLE;    
+                break;
             case BITRIS_CLEARING:       // Clearing lines
             case BITRIS_GAMEOVER:       // Game over
             default:
@@ -65,6 +93,7 @@ void vBitrisTask(void *pvParameters){
 
 void RunApp(void)
 {
+    bitris = BitrisInit();
     xTaskCreate(vBitrisTask, "Bitris", 256, NULL, 2, NULL);
     Button_Init(&sw2, BUTTON_GPIO_Port, SW2_Pin, ButtonHandler);
     Button_Init(&sw3, BUTTON_GPIO_Port, SW3_Pin, ButtonHandler);
