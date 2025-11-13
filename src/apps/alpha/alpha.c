99ec6fb0233f585fe56b9e6bd9ffa8b166f96421
Alejo Rodrigo Campos <ljcmps01@gmail.com>
2025-11-13 00:12:24 -0300

    fix: infinite loop bug

--- src/apps/alpha/alpha.c
+++ src/apps/alpha/alpha.c
@@ -5,6 +5,34 @@ Button sw2,sw3;
 
 uint8_t counter=0;
 
+int get_char(char c){
+    if(c>='A' && c<='Z'){
+        return c-'A';
+    }
+    else if(c>='a' && c<='z'){
+        return c-'A'-6;
+    }
+    else{
+        return -1; //caracter no soportado
+    }
+}
+
+void load_char_to_screen(char* c){
+    Matrix_t *m = GetMatrix();
+    int a=3;
+    for(int i=0; i<a; i++){
+        load_output(m,letters[counter]);
+        SEGGER_RTT_printf(0, "Loading char: %c\n", c[i]);
+        vTaskDelay(pdMS_TO_TICKS(500));
+        counter=get_char(c[i]);
+        SEGGER_RTT_printf(0, "Loading char: %d\n", counter);
+        if(counter!=-1){
+            load_output(m,letters[counter]);
+            vTaskDelay(pdMS_TO_TICKS(500));
+        }
+    }
+}
+
 void ButtonHandler(const Button *btn, ButtonEvent_t event)
 {
     Matrix_t *matrix = GetMatrix();
@@ -48,37 +76,20 @@ void ButtonHandler(const Button *btn, ButtonEvent_t event)
     load_output(matrix,letters[counter]);
 }
 
+void vAlphaTask(void *pvParameters)
+{
+    load_char_to_screen("HolA MuNdO");
+    
+    // After displaying, you can loop or delete the task
+    for(;;) {
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
 void RunApp(void)
 {
     Button_Init(&sw2, BUTTON_GPIO_Port, SW2_Pin, ButtonHandler);
     Button_Init(&sw3, BUTTON_GPIO_Port, SW3_Pin, ButtonHandler);
-    //load_char_to_screen("jhh");
-}
 
-void load_char_to_screen(char* c){
-    Matrix_t *m = GetMatrix();
-    int a=3;
-    for(int i=0; i<a; i++){
-        load_output(m,letters[counter]);
-        SEGGER_RTT_printf(0, "Loading char: %c\n", c[i]);
-        vTaskDelay(pdMS_TO_TICKS(500));
-        counter=get_char(c[i]);
-        SEGGER_RTT_printf(0, "Loading char: %d\n", counter);
-        if(counter!=-1){
-            load_output(m,letters[counter]);
-            vTaskDelay(pdMS_TO_TICKS(500));
-        }
-    }
-}
-
-int get_char(char c){
-    if(c>='A' && c<='Z'){
-        return c-'A';
-    }
-    else if(c>='a' && c<='z'){
-        return c-'A'-6;
-    }
-    else{
-        return -1; //caracter no soportado
-    }
+    xTaskCreate(vAlphaTask, "AlphaTask", 128, NULL, tskIDLE_PRIORITY + 1, NULL);
 }
